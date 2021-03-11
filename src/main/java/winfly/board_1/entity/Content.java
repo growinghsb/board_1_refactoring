ecdd67136b0eef6b481fdde22dfe87c58d0fab51
growinghsb <seungbeombm@gmail.com>
2021-03-11 13:21:03 +0900

    리팩토링 끝

--- src/main/java/winfly/board_1/entity/Content.java
+++ src/main/java/winfly/board_1/entity/Content.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
-@Entity @Getter @Setter
+@Entity @Getter
 // 생성자를 통한 무분별한 생성을 막기 위해 protected 기본 생성자를 만들 것, jpa 특성상 접근 제어자를 protected까지 허용.
 // setter를 없애고, 비즈니스 메서드를 통해 하나의 메서드를 통해 create작업 진행할 것.
 public class User {
